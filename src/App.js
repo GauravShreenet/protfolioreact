a508895478e8b612b78ddcaad710c7807015d700
GauravShreenet <gthakuri656@gmail.com>
2023-10-10 14:40:44 +1100

    adding more component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import { Hero } from './components/Hero';
 import { Indo } from './components/Indo';
+import { Links } from './components/Links';
 import { NavBar } from './components/NavBar';
 import { Project } from './components/Project';
 import { Skills } from './components/Skills';
 
 function App() {
   return (
+    <>
+    
     <div className="wrapper">
       <NavBar />
       <Hero />
@@ -15,7 +20,11 @@ function App() {
       <Skills />
       <Project />
       <About />
+      <Contact />
+      <Links />
+      <Footer />
     </div>
+    </>
   );
 }
 
